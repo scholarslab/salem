dce1c788f4e582f20b0b62e133be0a29f7e5b662
ewg4x <ewg4x@07c41579-21cc-4512-95e7-b8629c30e65c>
2009-03-30 15:06:46 +0000

    adding saxon and indexing script to indexer folder
    git-svn-id: file:///tmp/salem@34 07c41579-21cc-4512-95e7-b8629c30e65c

---
+++ trunk/cocoon/indexer/generate_index.sh
@@ -0,0 +1,16 @@
+java -jar saxon/saxon9.jar -xi:on -s ../cocoon/xml/BoySal1R.xml -xsl:add_docs.xsl > index/add_BoySal1R.xml;
+java -jar saxon/saxon9.jar -xi:on -s ../cocoon/xml/BoySal2R.xml -xsl:add_docs.xsl > index/add_BoySal2R.xml;
+java -jar saxon/saxon9.jar -xi:on -s ../cocoon/xml/BoySal3R.xml -xsl:add_docs.xsl > index/add_BoySal3R.xml;
+
+FILE="index/add_*.xml"
+URL=http://127.0.0.1:8983/solr/salem/update
+
+for f in $FILE; do
+  echo Posting file $f to $URL
+  curl $URL --data-binary @$f -H 'Content-type:text/xml; charset=utf-8' 
+  echo
+done
+
+#send the commit command to make sure all the changes are flushed and visible
+curl $URL --data-binary '<commit/>' -H 'Content-type:text/xml; charset=utf-8'
+echo
