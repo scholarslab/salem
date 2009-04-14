f11adc8c21472c6e3b4f760ca2542176cca8fe92
ewg4x <ewg4x@07c41579-21cc-4512-95e7-b8629c30e65c>
2009-04-14 14:08:32 +0000

    added echo for transforming commands
    git-svn-id: file:///tmp/salem@40 07c41579-21cc-4512-95e7-b8629c30e65c

--- trunk/branch/indexer/generate_index.sh
+++ trunk/branch/indexer/generate_index.sh
@@ -1,5 +1,8 @@
+echo Generating add_doc for BoySal1R.xml;
 java -jar saxon/saxon9.jar -xi:on -s ../cocoon/xml/BoySal1R.xml -xsl:add_docs.xsl > index/add_BoySal1R.xml;
+echo Generating add_doc for BoySal2R.xml;
 java -jar saxon/saxon9.jar -xi:on -s ../cocoon/xml/BoySal2R.xml -xsl:add_docs.xsl > index/add_BoySal2R.xml;
+echo Generating add_doc for BoySal3R.xml;
 java -jar saxon/saxon9.jar -xi:on -s ../cocoon/xml/BoySal3R.xml -xsl:add_docs.xsl > index/add_BoySal3R.xml;
 
 FILE="index/add_*.xml"
