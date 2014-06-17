b2dc1f1ef395e5deda1a51cd44f058930f7d8205
wsg4w <wsg4w@07c41579-21cc-4512-95e7-b8629c30e65c>
2014-06-17 18:26:17 +0000

    Rename nodes for volume 3
    git-svn-id: file:///tmp/salem@343 07c41579-21cc-4512-95e7-b8629c30e65c

---
+++ trunk/branch/cocoon/xml/rename-nodes.rb
@@ -0,0 +1,40 @@
+#! /usr/bin/env ruby
+
+require 'nokogiri'
+require 'awesome_print'
+
+
+def open_file(source)
+  file = File.open(source)
+  @doc = Nokogiri::XML(file)
+  file.close
+end
+
+def rename
+
+  @doc.xpath('//div1').each do |div1|
+    parent_id = div1.attr('id').gsub(/BoySal3/, 'swp')
+
+    div1['id'] = parent_id
+
+    div2_counter = 1
+
+    div1.xpath('./div2').each do |div2|
+      doc_id = "#{parent_id}.#{div2_counter}"
+      #ap "#{parent_id}: #{doc_id} | #{current_id}"
+
+      #current_id = div2['id']
+      #div2['data-id'] = current_id
+      div2['id'] = doc_id
+
+      div2_counter += 1
+    end
+
+  end
+
+end
+
+open_file('./volume3.xml')
+rename
+ap @doc
+
