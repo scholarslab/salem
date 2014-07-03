3aa85e99509c612194ca7e12f048b1d5e17375d2
wsg4w <wsg4w@07c41579-21cc-4512-95e7-b8629c30e65c>
2014-07-03 13:25:59 +0000

    Clean up index directory before generating the index and the actual index
    git-svn-id: file:///tmp/salem@360 07c41579-21cc-4512-95e7-b8629c30e65c

--- trunk/branch/indexer/generate_index.sh
+++ trunk/branch/indexer/generate_index.sh
@@ -4,8 +4,9 @@ SERVER=`uname -n`
 FILE="index/add_*.xml"
 URL=http://localhost:8080/solr/salem/update
 
-echo Indexing for Solr server $URL
-
+echo Cleaning up
+`rm -f index/add_*.xml`
+`curl $URL?commit=true -H "Content-Type: text/xml" --data-binary '<delete><query>*:*</query></delete>'`
 
 #echo Generating add_doc for BoySal1R.xml;
 #java -jar saxon/saxon9.jar -xi:on -s ../cocoon/xml/BoySal1R.xml -xsl:add_docs.xsl > index/add_BoySal1R.xml;
