8fd2759f9ed610fbe5d159155a7c72afb00cb1a5
wsg4w <wsg4w@07c41579-21cc-4512-95e7-b8629c30e65c>
2014-08-21 13:22:43 +0000

    Updates from Joseph Stuart
    git-svn-id: file:///tmp/salem@368 07c41579-21cc-4512-95e7-b8629c30e65c

--- trunk/branch/cocoon/xml/rename-nodes.rb
+++ trunk/branch/cocoon/xml/rename-nodes.rb
@@ -2,6 +2,7 @@
 
 require 'nokogiri'
 require 'awesome_print'
+require 'csv'
 
 
 def open_file(source)
@@ -12,29 +13,36 @@ end
 
 def rename
 
-  @doc.xpath('//div1').each do |div1|
-    parent_id = div1.attr('id').gsub(/BoySal3/, 'swp')
+  CSV.open('map.csv', 'w') do |csv|
 
-    div1['id'] = parent_id
+    csv << ['parent_id', 'new_id', 'old_id', 'name']
 
-    div2_counter = 1
+    @doc.xpath('//div1').each do |div1|
+      parent_id = div1.attr('id').gsub(/BoySal3/, 'swp')
 
-    div1.xpath('./div2').each do |div2|
-      doc_id = "#{parent_id}.#{div2_counter}"
-      #ap "#{parent_id}: #{doc_id} | #{current_id}"
+      div1['id'] = parent_id
+      name = div1.xpath('./head/name').text
+      ap name
+      div2_counter = 1
 
-      #current_id = div2['id']
-      #div2['data-id'] = current_id
-      div2['id'] = doc_id
+      div1.xpath('./div2').each do |div2|
+        doc_id = "#{parent_id}.#{div2_counter}"
+        current_id = div2['id']
+        #ap "#{parent_id}, #{doc_id}, #{current_id}", "#{name}"
+        csv << [parent_id, doc_id, current_id, name]
 
-      div2_counter += 1
+        #      #div2['data-id'] = current_id
+        #div2['id'] = doc_id
+
+        div2_counter += 1
+      end
+
     end
-
   end
 
 end
 
-open_file('./swp.xml')
+open_file('./swp-old-ids.xml')
 rename
-ap @doc
+#ap @doc
 
